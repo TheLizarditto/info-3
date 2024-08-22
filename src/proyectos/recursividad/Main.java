91310deb14e46a2e29f613a544e50d764a9cf893
Matías <matias.haron@gmail.com>
2024-08-22 11:15:40 -0300

    Implementamos la sucesion de fibonacci

--- src/proyectos/recursividad/Main.java
+++ src/proyectos/recursividad/Main.java
@@ -7,8 +7,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Fact factorial = new Fact();
         Sum sumatoria = new Sum();
+        Fib fibonacci = new Fib();
+
         do {
-            System.out.println("Seleccione: \n1. Factorial \n2. Sumatoria \n0. Salir");
+            System.out.println("Seleccione: \n1. Factorial \n2. Sumatoria \n3. Fibonacci \n0. Salir");
             sel = input.nextLine();
     
             try {
@@ -28,6 +30,15 @@ public class Main {
                         res = input.nextLine();
                         try {
                             System.out.println(sumatoria.sum(Integer.parseInt(res)));
+                        } catch (Exception e) {
+                            System.out.println("Debe ingresar un numero valido");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Ingrese un numero: ");
+                        res = input.nextLine();
+                        try {
+                            System.out.println(fibonacci.fib(Integer.parseInt(res)));
                         } catch (Exception e) {
                             System.out.println("Debe ingresar un numero valido");
                         }
